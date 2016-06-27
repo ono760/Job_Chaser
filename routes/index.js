6a30c519337775f39b10badd4e6be7b297c92d95
James Lim <lamesjim@Jamess-MacBook-Pro.local>
2016-06-27 14:47:21 -0700

    login, cookies, signup working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
+"use strict";
 var express = require('express');
 var router = express.Router();
+var pg = require("pg");
+var knex = require("../db/knex");
+var bcrypt = require("bcrypt");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
