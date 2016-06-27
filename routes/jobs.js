17bd6d02544a4d9b60abf8f1ab1384428a37b000
James Lim <lamesjim@Jamess-MacBook-Pro.local>
2016-06-27 16:33:08 -0700

    don't merge this

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function(req, res, next) {
+    console.log(req.query)
     let title = req.query.title;
     let location = req.query.location;
     request(`http://api.indeed.com/ads/apisearch?publisher=4710753624090411&q=${title}&l=${location}&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json`,function(err,response,data){
