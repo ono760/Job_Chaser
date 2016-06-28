98058bf03ccb252e69bf067c04372e5d92f0e79f
Onofre Echeverria <ono.ucsb@gmail.com>
2016-06-28 11:53:42 -0700

    pagination in progress

--- routes/jobs.js
+++ routes/jobs.js
@@ -6,11 +6,19 @@ var request = require('request');
 router.get('/', function(req, res, next) {
     let title = req.query.title;
     let location = req.query.location;
-    request(`http://api.indeed.com/ads/apisearch?publisher=4710753624090411&q=${title}&l=${location}&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json`,function(err,data){
-        console.log(err);
+    let page = req.query.start;
+    let currentQuery = JSON.stringify(req.query);
+    request(`http://api.indeed.com/ads/apisearch?publisher=4710753624090411&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json`,function(err,data){
         var jobs = JSON.parse(data.body).results;
-        res.render('index', {err:err, jobs:jobs});
+        res.render('index', {err:err, jobs:jobs, currentQuery:currentQuery});
     });
 });
 
+
+
+
+
+
 module.exports = router;
+
+
