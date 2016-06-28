e2c4d7b77fabe5d5b8897c59e425fe6980fdd193
James Lim <lamesjim@Jamess-MacBook-Pro.local>
2016-06-28 15:52:48 -0700

    pagination working for foundation, but need to readjust for materialize

--- routes/jobs.js
+++ routes/jobs.js
@@ -9,7 +9,6 @@ router.get('/', function(req, res, next) {
     let page = req.query.page || 1;
     let currentQuery = req.query
     let offset = parseInt(page)*10
-
     request(`http://api.indeed.com/ads/apisearch?publisher=4710753624090411&sort=&radius=&st=&jt=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json&q=${title}&l=${location}&start=${offset}`,function(err,data){
         let jobs = JSON.parse(data.body).results;
         let totalPages = Math.floor((JSON.parse(data.body).totalResults)/10);
