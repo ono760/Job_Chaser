e5e495a6a9ffcac2004d3dbc064b7fe08ec93774
Onofre Echeverria <ono.ucsb@gmail.com>
2016-06-28 14:14:34 -0700

    added oauth. need to deploy to start heroku deployment phase

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ var bcrypt = require("bcrypt");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log(req.user);
   res.render('index', { title: 'Job Search' });
 });
+
 
 module.exports = router;
