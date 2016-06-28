1ba5fcf688dd1d2735985d339a9f096f0d5e8fea
Onofre Echeverria <ono.ucsb@gmail.com>
2016-06-28 16:56:38 -0700

    added cookies

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var bcrypt = require("bcrypt");
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log(req.user);
-  res.render('index', { title: 'Job Search', user:req.user});
+  res.render('index', { title: 'Job Search'});
 
 });
 
