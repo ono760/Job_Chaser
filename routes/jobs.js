8c61c848e2a32b440143eab2b4737d9a33b7f8e5
James Lim <lamesjim@Jamess-MacBook-Pro.local>
2016-06-28 14:13:39 -0700

    pagination 95% done

--- routes/jobs.js
+++ routes/jobs.js
@@ -6,11 +6,20 @@ var request = require('request');
 router.get('/', function(req, res, next) {
     let title = req.query.title;
     let location = req.query.location;
-    let page = req.query.start;
-    let currentQuery = JSON.stringify(req.query);
-    request(`http://api.indeed.com/ads/apisearch?publisher=4710753624090411&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json`,function(err,data){
-        var jobs = JSON.parse(data.body).results;
-        res.render('index', {err:err, jobs:jobs, currentQuery:currentQuery});
+    let page = req.query.page || 1;
+    let currentQuery = req.query
+    let offset = parseInt(page)*10
+
+    request(`http://api.indeed.com/ads/apisearch?publisher=4710753624090411&sort=&radius=&st=&jt=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json&q=${title}&l=${location}&start=${offset}`,function(err,data){
+        let jobs = JSON.parse(data.body).results;
+        let totalPages = Math.floor((JSON.parse(data.body).totalResults)/10);
+        console.log(JSON.parse(data.body).start, 'start')
+        res.render('index', {err:err,
+                            jobs:jobs,
+                            currentQuery:currentQuery,
+                            page:page,
+                            totalPages:totalPages
+                          });
     });
 });
 
@@ -20,5 +29,3 @@ router.get('/', function(req, res, next) {
 
 
 module.exports = router;
-
-
