4a8e0db558229e1353f9b33730b7c1ce93ba7703
James Lim <lamesjim@Jamess-MacBook-Pro.local>
2016-06-27 18:29:20 -0700

    display api results

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
     let title = req.query.title;
     let location = req.query.location;
     request(`http://api.indeed.com/ads/apisearch?publisher=4710753624090411&q=${title}&l=${location}&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json`,function(err,response,data){
-        console.log(data.results[0]);
+        console.log(data);
         res.render('index', {err:err, data:data});
     });
 
