b6ca04a8fe04de734854290c068331a100d2b946
James Lim <james.albert.lim@gmail.com>
2016-06-29 11:49:07 -0700

    switch to ejs, pagination working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,22 +2,6 @@ $(function(){
     "use strict";
     $('.job-li').click(function(e){
         e.preventDefault();
-        $.ajax({
-            // url: 'http://api.indeed.com/ads/apisearch?publisher=4710753624090411&sort=&radius=&st=&jt=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json&q=java&l=san+jose&start=',
-            url: 'http://api.indeed.com/ads/apisearch?publisher=4710753624090411&q=java&l=san%20francisco%2C+ca&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json',
-            method: "GET",
-            dataType: "json",
-            // Headers: { "HeaderName": process.env },
-            success: function (data){
-              if (!data.Error) {
-                console.log(JSON.stringify(data));
-                $('.modalH4').html('ModalHeader');
-                $('.modalP').html('bunch of text');
-              } else {
-                  alert('Movie not found');
-              }
-            }
-        });
     });
     $('.modal-trigger').leanModal();
 });
