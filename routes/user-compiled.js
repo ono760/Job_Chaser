90ae4d472f97159693d40428279e16b11257d4f3
Nikhil <nikhilrampal15@gmail.com>
2016-06-28 19:53:49 -0700

    new branch

---
+++ routes/user-compiled.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+var pg = require("pg");
+var knex = require("../db/knex");
+var bcrypt = require("bcrypt");
+
+/* GET users listing. */
+router.get('/:id', function (req, res, next) {
+  // let id = req.session.id;
+  let id = 1;
+  knex('users').select('users.first_name', 'jobs.position', 'companies.name', 'user_jobs.status', 'user_jobs.notes', 'jobs.salary').join('user_jobs', 'user_jobs.user_id', 'users.id').join('jobs', 'user_jobs.job_id', 'jobs.id').join('companies', 'jobs.company_id', 'companies.id').where('users.id', id).then(function (info) {
+    res.json(info);
+  });
+});
+
+module.exports = router;
+
+//# sourceMappingURL=user-compiled.js.map
