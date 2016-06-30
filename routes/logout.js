3c8c0fb202a87e144d130fb2cc995601ae80c093
Nikhil <nikhilrampal15@gmail.com>
2016-06-29 18:37:17 -0700

    cookies not working

--- routes/logout.js
+++ routes/logout.js
@@ -1,23 +1,28 @@
-
-
+/**
+ * Created by Nikhil on 6/29/16.
+ */
 "use strict";
 var express = require('express');
 var router = express.Router();
 var pg = require("pg");
 var knex = require("../db/knex");
 var bcrypt = require("bcrypt");
+var cookieParser = require('cookie-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Job Search', 
-                        currentQuery: {},
-                        user:req.session.passport.user,
-                        jobs: null,
-                        page: null,
-      totalPages:null
-                      });
+    // res.render('index', { title: 'Job Search',
+    //     currentQuery: {},
+    //     user:req.session.passport.user,
+    //     jobs: null,
+    //     page: null,
+    //     totalPages:null
+    // });
+    res.redirect('/login')
 
 });
+
+
 // router.get('/', function(req, res){
 //     console.log(req.session.passport.user);
 //     res.render('signup',{user:req.session.passport.user});
