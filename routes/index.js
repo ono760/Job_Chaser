8d3bb83bc1c5c131489aba2b510553e2871cc1b4
Nikhil <nikhilrampal15@gmail.com>
2016-06-30 09:11:49 -0700

    'fixed partials ?'

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,22 @@ var bcrypt = require("bcrypt");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log(req.session.user,'hi');
+    console.log(req.session.passport,'bye');
+    // var tempUser;
+    // if (req.session.user) {
+    //     tempUser = req.session.user;
+    // }
+    // if (req.session.passport.user) {
+    //     tempUser = req.session.passport.user;
+    // } else {
+    //     tempUser = 'nik'
+    // }
+    // console.log('skdhalsd')
+
   res.render('index', { title: 'Job Search', 
                         currentQuery: {},
-                        user:req.session.passport.user,
+                        user:null,
                         jobs: null,
                         page: null,
       totalPages:null
