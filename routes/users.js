b5403406b31841fb95f7b736b1ab9ff159c43f05
James Lim <james.albert.lim@gmail.com>
2016-06-30 11:53:55 -0700

    crazy knex statement working, all relevant user/job info, edited

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ var bcrypt = require("bcrypt");
 router.get('/:id', function(req, res, next) {
   let id = req.params.id;
   knex('users').select().where('users.id', '=', id).then(function(user) {
-    knex('user_jobs').select('companies.id as company_id', 'jobs.id as job_id', 'jobs.position', 'companies.name', 'companies.location').distinct('user_jobs.id')
+    knex('user_jobs').select('companies.id as company_id', 'companies.name', 'companies.location').distinct('user_jobs.id')
     .innerJoin('jobs', 'user_jobs.job_id', 'jobs.id')
     .leftJoin('companies', 'jobs.company_id', 'companies.id')
     .where('user_jobs.user_id', "=", id)
@@ -31,6 +31,7 @@ router.get('/:id', function(req, res, next) {
         companies.forEach(function(company){
           companyMap[company.company_id] = company;
         });
+        console.log(companyMap)
 
         knex('user_jobs').select('user_jobs.id as user_jobs_id', 'jobs.id as jobs_id', 'companies.id as company_id', 'jobs.position', 'jobs.link_to_application', 'companies.name').distinct('user_jobs.id')
         .innerJoin('jobs', 'user_jobs.job_id', 'jobs.id')
