537a4656543b207b889bbded4f19d2b927b154f6
Onofre Echeverria <ono.ucsb@gmail.com>
2016-07-01 11:54:51 -0700

    added css and modified some linked in

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -7,6 +7,9 @@ exports.up = function(knex, Promise) {
           table.string('first_name').notNullable();
           table.string('last_name').notNullable();
           table.string('email').unique().notNullable();
+
+          table.string('linkedin_id').unique(); //added
+
           table.string('password').notNullable();
           table.biginteger('phone_number',20);
       }),
